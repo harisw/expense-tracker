23a487a217b556542318511291be5b1163fdf471
Hari <harrysw6@gmail.com>
2026-03-13 17:57:06 +0900

    chore: add csrf

--- src/main/java/com/harisw/springexpensetracker/config/SecurityConfig.java
+++ src/main/java/com/harisw/springexpensetracker/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.harisw.springexpensetracker.config;
 import com.harisw.springexpensetracker.infrastructure.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -23,7 +24,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf->csrf.disable())
+                .cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
--- src/main/java/com/harisw/springexpensetracker/config/WebConfig.java
+++ src/main/java/com/harisw/springexpensetracker/config/WebConfig.java
@@ -1,13 +1,23 @@
 package com.harisw.springexpensetracker.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${client.origins}")
+    private String[] clientOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowedMethods("*");
+
+        registry.addMapping("/api/**")
+                .allowedOrigins(clientOrigins)
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
